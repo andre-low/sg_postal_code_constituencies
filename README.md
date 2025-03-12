1849acc0197e89b00b0da29787d70286975a100e
horatiojh <horatio.j.ho@gmail.com>
2025-03-12 15:05:23 +0800

    add onemap call

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ Possible approaches:
 * [OneMap API](https://www.onemap.gov.sg/apidocs/): Can return lat-long from postal code
     * [Search API](https://www.onemap.gov.sg/apidocs/search)
 * [ELD Election Boundary 2020 dataset](https://data.gov.sg/datasets/d_6077aa5ab73d447b32f451ea224221b6/view)
+* [ELD Election Boundary 2025 dataset](https://data.gov.sg/datasets/d_7ddf956dfc1c59080bf95bba1c58a5d2/view)
 
 ## Potentially useful
 * [StackOverflow: Check if geo-point is inside or outside of polygon](https://stackoverflow.com/questions/43892459/check-if-geo-point-is-inside-or-outside-of-polygon)
---
+++ get_postal_code.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv
+import json
+
+# Load environment variables from the .env file
+load_dotenv()
+
+def get_coord_from_pc(pc):
+    url = "https://www.onemap.gov.sg/api/common/elastic/search?searchVal={}&returnGeom=Y&getAddrDetails=Y&pageNum=1".format(pc)
+
+    bearer = "Bearer {}".format(os.getenv('OPENAPI_TOKEN'))
+        
+    headers = {"Authorization": bearer}
+        
+    response = requests.get(url, headers=headers)
+        
+    print(response.text)
+    data = json.loads(response.text)
+    lat = data['results'][0]['LATITUDE']
+    lon = data['results'][0]['LONGITUDE']
+
+    # return lat and lon
+    return (lat, lon)
+
+# start with a postal code 
+postal_code = 544186
+
+# convert it to lat/long using OneMap API
+coordinates = get_coord_from_pc(postal_code)
+print(coordinates)
+
+
+
